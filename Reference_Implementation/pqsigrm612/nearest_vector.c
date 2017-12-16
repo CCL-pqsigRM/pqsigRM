1ea3451b47d7d658134065e917d97921eeefe7fd
Yongwoo Lee <yongwool@ccl.snu.ac.kr>
2017-12-17 00:22:15 +0900

    nearest vector index

--- Reference_Implementation/pqsigrm612/nearest_vector.c
+++ Reference_Implementation/pqsigrm612/nearest_vector.c
@@ -6,50 +6,48 @@ int* reculsive_decoding(float* y, int r1, int m1, int f, int l) {
 		int a1 = 0;
 		int a2 = 0;
 
-		for (int i = f; i < l + 1; i++) {
+		for (int i = f; i < l; i++) {
 			a1 = a1 + pow(y[i] - 1, 2);
 			a2 = a2 + pow(y[i] + 1, 2);
 		}
 		if (a1 <= a2) {
-			for (int i = f; i < l + 1; i++) {
+			for (int i = f; i < l; i++) {
 				y[i] = 1;
 			}
 		}
 		else if (a1 > a2) {
-			for (int i = f; i < l + 1; i++) {
+			for (int i = f; i < l; i++) {
 				y[i] = -1;
 			}
 		}
 
 	}
 	else if (r1 == m1) {
-		for (int i = f; i < l + 1; i++) {
+		for (int i = f; i < l; i++) {
 			if (y[i] >= 0) { y[i] = 1; }
 			else if (y[i] < 0) { y[i] = -1; }
 		}
 	}
 	else {
-		float *temp = (float*)malloc(sizeof(float)* ((l - f + 1) / 2));
-//		double *temp = new double [(l - f + 1) / 2];
-		//temp¿¡ ¿Å±è
-		for (int i = 0; i < (l - f + 1) / 2; i++) {
-			temp[i] = y[i + (l + f + 1) / 2];
+		float *temp = (float*)malloc(sizeof(float)* ((l - f) / 2));
+		for (int i = 0; i < (l - f) / 2; i++) {
+			temp[i] = y[i + (l + f) / 2];
 		}
 
-		for (int i = 0; i < (l - f + 1) / 2; i++) {
-			y[i + (l + f + 1) / 2] = y[i + (l + f + 1) / 2] * y[i + f];
+		for (int i = 0; i < (l - f) / 2; i++) {
+			y[i + (l + f) / 2] = y[i + (l + f) / 2] * y[i + f];
 		}
 
-		reculsive_decoding(y, r1 - 1, m1 - 1, (l + f + 1) / 2, l);
+		reculsive_decoding(y, r1 - 1, m1 - 1, (l + f) / 2, l);
 
-		for (int i = 0; i < (l - f + 1) / 2; i++) {
-			y[f + i] = (y[f + i] + y[i + (l + f + 1) / 2] * temp[i]) / 2;
+		for (int i = 0; i < (l - f) / 2; i++) {
+			y[f + i] = (y[f + i] + y[i + (l + f) / 2] * temp[i]) / 2;
 		}
 
-		reculsive_decoding(y, r1, m1 - 1, f, (l + f - 1) / 2);
+		reculsive_decoding(y, r1, m1 - 1, f, (l + f) / 2);
 
-		for (int i = 0; i < (l - f + 1) / 2; i++) {
-			y[i + (l + f + 1) / 2] = y[i + (l + f + 1) / 2] * y[i + f];
+		for (int i = 0; i < (l - f) / 2; i++) {
+			y[i + (l + f) / 2] = y[i + (l + f) / 2] * y[i + f];
 		}
 
 		free(temp);
@@ -63,5 +61,5 @@ void nearest_vector(float* y){
 	// I. Dumer, “Recursive Decoding and Its Performance for Low-Rate 
 	// Reed-Muller Codes,” IEEE Trans. Inform. Theory, may 2004.
 	// 
-	reculsive_decoding(y, RM_R, RM_M, 0, CODE_N -1);
+	reculsive_decoding(y, RM_R, RM_M, 0, CODE_N);
 }
