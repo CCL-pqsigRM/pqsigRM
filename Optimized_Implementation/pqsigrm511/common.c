13194456b361ea7b0c8aab21c283417229481b22
yongwool <34329734+yongwool@users.noreply.github.com>
2017-12-18 17:26:27 +0900

    Update common.c

--- Optimized_Implementation/pqsigrm511/common.c
+++ Optimized_Implementation/pqsigrm511/common.c
@@ -4,14 +4,14 @@
 unsigned char* hashMsg(unsigned char *s, const unsigned char *m, 
 	unsigned long long mlen, unsigned long long sign_i){
 	// Hash the given message
-	// syndrome s = h(h(M)|i) | (h(h(M)|i)|i) | ...
+	// syndrome s = h(h(M)|i) | (h(h(M)|i)) | ...
 
-	*(unsigned long long*)(s+HASHSIZEBYTES) = sign_i;// concatenate i i.e. h(h(M)|i)
+	*(unsigned long long*)(s+HASHSIZEBYTES) = sign_i;// concatenate i i.e. h(M)|i
 	
 	SHA512(m, mlen, s);
 	SHA512(s, HASHSIZEBYTES+sizeof(unsigned long long), s); //h(h(M)|i)
 	
-	SHA512(s, HASHSIZEBYTES, s+HASHSIZEBYTES);//(h(h(M)|i)|i)
+	SHA512(s, HASHSIZEBYTES, s+HASHSIZEBYTES);//(h(h(M)|i))
 	
 	return s;
 }
