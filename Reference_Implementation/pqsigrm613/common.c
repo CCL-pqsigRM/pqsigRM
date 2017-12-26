3e5cef8d481f579959a9afbe54fcab95fc6fb1fc
Yongwoo Lee <yongwool@ccl.snu.ac.kr>
2017-12-26 16:03:27 +0900

    pqsigrm412 add

--- Reference_Implementation/pqsigrm613/common.c
+++ Reference_Implementation/pqsigrm613/common.c
@@ -3,9 +3,7 @@
 
 unsigned char* hashMsg(unsigned char *s, const unsigned char *m, 
 	unsigned long long mlen, unsigned long long sign_i){
-	// Assuming 64bit environment, say size_t is defined as unsigned long
-	// Hash the given message
-	// syndrome s = h(h(M)|i)|(h(h(M)|i))
+	// syndrome s = h(h(M)|i)|(h(h(M)|i))|...
 	SHA512(m, mlen, s);
 	*(unsigned long long*)(s+HASHSIZEBYTES) = sign_i;// concatenate i i.e. h(M)|i
 	SHA512(s, HASHSIZEBYTES+sizeof(unsigned long long), s); //h(h(M)|i)
