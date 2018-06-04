5714ae1ae5cdfa34a153097d2078e2fa41b3beb0
Yongwoo Lee <yongwool@ccl.snu.ac.kr>
2018-06-04 11:47:51 +0900

    rng using AES

--- Reference_Implementation/pqsigrm511/rng.c
+++ Reference_Implementation/pqsigrm511/rng.c
@@ -7,12 +7,9 @@
 
 #include <string.h>
 #include "rng.h"
-#include <stdlib.h>
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
-
-#include <stdlib.h>
 
 AES256_CTR_DRBG_struct  DRBG_ctx;
 
@@ -158,10 +155,33 @@ randombytes_init(unsigned char *entropy_input,
 int
 randombytes(unsigned char *x, unsigned long long xlen)
 {
-   	unsigned* xx = (unsigned*)x; 
-	for(unsigned long long i =0; i< xlen/sizeof(unsigned); i++){
-		xx[i] = rand();
-	}
+    unsigned char   block[16];
+    int             i = 0;
+    
+    while ( xlen > 0 ) {
+        //increment V
+        for (int j=15; j>=0; j--) {
+            if ( DRBG_ctx.V[j] == 0xff )
+                DRBG_ctx.V[j] = 0x00;
+            else {
+                DRBG_ctx.V[j]++;
+                break;
+            }
+        }
+        AES256_ECB(DRBG_ctx.Key, DRBG_ctx.V, block);
+        if ( xlen > 15 ) {
+            memcpy(x+i, block, 16);
+            i += 16;
+            xlen -= 16;
+        }
+        else {
+            memcpy(x+i, block, xlen);
+            xlen = 0;
+        }
+    }
+    AES256_CTR_DRBG_Update(NULL, DRBG_ctx.Key, DRBG_ctx.V);
+    DRBG_ctx.reseed_counter++;
+    
     return RNG_SUCCESS;
 }
 
